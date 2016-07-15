c042ed9e4e04ec014e5ea8ec3d95d203af5bea4f
vgoel30 <varun.goel@stonybrook.edu>
2016-07-16 04:29:45 +0530

    Checks to see if the user has provided a float value for indent

--- document_editor.py
+++ document_editor.py
@@ -11,7 +11,7 @@ def modify_indents(filename, indent_amount):
 
     #iterate over all the paragraphs in the document
     for paragraph in doc.paragraphs:
-    	#delete all the leading white space for new indent
+    	#delete all the leading white spaces for new indent
         paragraph.text = paragraph.text.lstrip()
         #modify the indentation 
         paragraph_format = paragraph.paragraph_format
@@ -19,10 +19,18 @@ def modify_indents(filename, indent_amount):
 
 	doc.save(filename)
 
+#if inadequate arguments are provided
 if(len(argv) != 2):
     print("\nERROR!\nSample Usage: python document_editor.py 2.5\n")
     quit()
 
+#get the command line arguments
 script, indent_amount = argv
 
+#ensure that the indent amount provided is a float
+if not isinstance(indent_amount, float):
+    print("Indent amount must be a float data type")
+    quit()
+
+#test the function
 modify_indents('sample.docx', float(indent_amount))
