d7fe344391dc7cae6c2c89a4bc7220e05ae771eb
vgoel30 <varun.goel@stonybrook.edu>
2016-07-30 18:21:04 +0530

    Liking now works

--- fb_like.py
+++ fb_like.py
@@ -1,26 +1,26 @@
 import fb
-#import facebook
 import json
 from facepy import GraphAPI
 
 token = 'EAAaReYTNsbQBAFPzZAolsUFGbkKDpUZBH5iWszmrvnkcGx236RMKy4yrLk2ZBlef1HN634N96EciaKZBIC48VuUaYFuBJj6KJF2ENDsgoaZCd9VocZBZCWuq23T7Kg1tS5iAfRhlfDlLFDULfFMivU2nVaZCipHuPDkZD'
-facebook = fb.graph.api(token)
+facebook_access = fb.graph.api(token)
 my_graph = GraphAPI(token)
 query = "/me/feed"
 
 feed = my_graph.get(query)
 #idlist = [x['message'] for x in feed['data']]
+
+#the list of all the messages (potential brithday wishes)
 message_list = []
+#filered posts will be identified by their id
+id_list = []
 
 for post in feed['data']:
 	try:
 		message = post['message']
 		message_list.append(json.dumps(message))
+		print(post['id'])
 	except:
 		pass
 
-print(message_list[0])
-
-# post = graph.get_object(id='908875805800760')
-# print(post['name'])
-#100000350486230
+facebook_access.publish(cat = "likes", id = '1168324629855875_1168367846518220')
