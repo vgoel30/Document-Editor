d36c74e48a8557c9f20dc216cf4f6e574638af5a
vgoel30 <varun.goel@stonybrook.edu>
2016-07-30 17:56:37 +0530

    Setup stuff for getting all the posts on the user's wall with a message

--- fb_like.py
+++ fb_like.py
@@ -3,18 +3,23 @@ import fb
 import json
 from facepy import GraphAPI
 
-token = 'EAACEdEose0cBAOTRwwHR2BajvknZBtfLdMsjJ8TCzhTWZAUlKPCzE0JaZAOt7UTiQu3fZAaVu1lxUkOWd8l1UKpJ8wzyHaOPWkZAwJKX5yv6j1R7saSrGJGsPZBC2th2JzsVzyhehPdhPumRRw3IqSWyAHN7C0cq6roljZCLp7eaQZDZD'
+token = 'EAAaReYTNsbQBAFPzZAolsUFGbkKDpUZBH5iWszmrvnkcGx236RMKy4yrLk2ZBlef1HN634N96EciaKZBIC48VuUaYFuBJj6KJF2ENDsgoaZCd9VocZBZCWuq23T7Kg1tS5iAfRhlfDlLFDULfFMivU2nVaZCipHuPDkZD'
 facebook = fb.graph.api(token)
 my_graph = GraphAPI(token)
 query = "/me/feed"
 
 feed = my_graph.get(query)
 #idlist = [x['message'] for x in feed['data']]
+message_list = []
 
 for post in feed['data']:
-	print(post['message'])
+	try:
+		message = post['message']
+		message_list.append(json.dumps(message))
+	except:
+		pass
 
-#print(idlist)
+print(message_list[0])
 
 # post = graph.get_object(id='908875805800760')
 # print(post['name'])
