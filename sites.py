33dcc30974740a9e96ea341271b3edad2ea5d855
vgoel30 <varun.goel@stonybrook.edu>
2016-11-13 18:10:22 -0500

    Removed redundant files

--- sites.py
+++
@@ -1,8 +0,0 @@
-import webbrowser
-
-#a list of the websites
-websites_list = ["http://team-bhp.com", "http://autocar.co.uk", "https://www.reddit.com/r/Jokes/"
-, "http://www.theverge.com", "http://www.topgear.com/car-news"]
-
-for address in websites_list:
-	webbrowser.open(address)
